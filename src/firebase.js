7e058be3246fc8aacc083441ab2091cfc5dcc1c3
mammothneck <siddarthaaggarwal@gmail.com>
2023-07-13 12:38:36 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-UVT_9U-ipoBth2jWOEVyAvFuFoX6naI",
-  authDomain: "chat-ca311.firebaseapp.com",
-  databaseURL: "https://chat-ca311-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chat-ca311",
-  storageBucket: "chat-ca311.appspot.com",
-  messagingSenderId: "452457944097",
-  appId: "1:452457944097:web:76f8308e7e4e83fbf1bf60",
-  measurementId: "G-720LK2ENVV"
+  apiKey: "AIzaSyBVsh6a0Szlkmj6iTCZYlBCpn8LPvqKtpU",
+  authDomain: "chat-24875.firebaseapp.com",
+  projectId: "chat-24875",
+  storageBucket: "chat-24875.appspot.com",
+  messagingSenderId: "368537588575",
+  appId: "1:368537588575:web:68e72f6eed5bb842921cea",
+  measurementId: "G-NTJ1JG0GFZ"
 };
 
 // Initialize Firebase
